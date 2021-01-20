7e1114a4f12195a3dbca013eafbe28760755d22d
maeso <tonymaeso11@gmail.com>
2021-01-20 14:07:55 +0100

    Prueba

--- EjercicioChat/src/Cliente2.java
+++ EjercicioChat/src/Cliente2.java
@@ -2,21 +2,19 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ClienteChat {
+public class Cliente2 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		Socket socket = null;
 		Socket socket2 = null;
 		BufferedReader bfr = null;
 		DataOutputStream oos = null;
 		DataInputStream isr = null;
 		try {
-			socket = new Socket("172.16.8.153", 9877);
-			socket2 = new Socket("172.16.8.138", 9877);
+			socket = new Socket("172.16.8.138", 9877);
+			socket2 = new Socket("172.16.8.153", 9877);
 			//Escribe el mensaje
 			oos = new DataOutputStream(socket.getOutputStream());
 			oos.writeUTF("Hola");
